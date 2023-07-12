9809cc01e473b0904239dcc3ad799f85593a11a9
sdaleshin <sdaleshin@gmail.com>
2023-07-12 17:18:51 +0200

    added test classes

--- dev-app/app.ts
+++ dev-app/app.ts
@@ -1,5 +1,13 @@
+import {autoinject} from "aurelia-framework";
+import {MainClass} from "resources";
+
+@autoinject
 export class App {
   public message = 'from Aurelia!';
+
+  constructor(private main: MainClass) {
+    this.message = this.main.greet('User')
+  }
 
   clicked(): void {
     // eslint-disable-next-line no-alert
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,29 @@
 import {FrameworkConfiguration} from 'aurelia-framework';
 import {PLATFORM} from 'aurelia-pal';
+import {autoinject} from 'aurelia-framework';
 
 export function configure(config: FrameworkConfiguration): void {
-  config.globalResources([
-    PLATFORM.moduleName('./elements/hello-world')
-  ]);
+  // config.globalResources([
+  //   PLATFORM.moduleName('./elements/hello-world')
+  // ]);
+}
+
+@autoinject
+export class UtilsClass {
+  prepareString(str: string){
+    return 'PPP ' + str
+  }
+}
+
+@autoinject
+export class MainClass {
+  utils: UtilsClass;
+
+  constructor(utils: UtilsClass) {
+    this.utils = utils
+  }
+
+  greet(name: string){
+    return this.utils.prepareString(`Hello ${name} from class`)
+  }
 }
